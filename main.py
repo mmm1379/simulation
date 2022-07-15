786d4342a84f72be5985ccc69ea72627db262686
mahdimostajer <mahdi.mostajeran.ce@gmail.com>
2022-07-15 17:12:38 +0430

    add services
    Co-authored-by: mmm1379 <mmm1379@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import enum
 import random
-import enum
+import numpy
 
 class requestName(enum.Enum):
     mobileRequest = 1
@@ -22,35 +22,37 @@ class microServices(enum.Enum):
     
         
         
-class Request():
-    def __init__(self, name:requestName, chain, probability) -> None:
+class TypeRequest():
+    def __init__(self, name: requestName, chain, probability) -> None:
         self.requestName = name
         self.chain = chain
         self.probability = probability
         
-Requests = [
-    Request(requestName.mobileRequest,
+typeRequests = [
+    TypeRequest(requestName.mobileRequest,
             [microServices.APIPort,microServices.orderManagement,microServices.payments]
             ,0.2),
-    Request(requestName.webRequest,
+    TypeRequest(requestName.webRequest,
             [microServices.webPort,microServices.orderManagement,microServices.payments]
             ,0.1),
-    Request(requestName.sendMessage,
+    TypeRequest(requestName.sendMessage,
             [microServices.APIPort,microServices.customerManagement,microServices.courierContact]
             ,0.05),
-    Request(requestName.restaurantMobile,
+    TypeRequest(requestName.restaurantMobile,
             [microServices.APIPort,microServices.restaurantManagement]
             ,0.25),
-    Request(requestName.restaurantWeb,
+    TypeRequest(requestName.restaurantWeb,
             [microServices.webPort,microServices.restaurantManagement]
             ,0.15),
-    Request(requestName.courierRequest,
+    TypeRequest(requestName.courierRequest,
             [microServices.webPort,microServices.restaurantManagement,microServices.courierContact]
             ,0.2),
-    Request(requestName.followUp,
+    TypeRequest(requestName.followUp,
             [microServices.APIPort,microServices.orderManagement]
             ,0.05)
 ]
+
+
 
 meanServiceTimes = {
     microServices.restaurantManagement: 8,
@@ -63,23 +65,63 @@ meanServiceTimes = {
 }
 
 
+
 requestProbs = [20,10,5,25,15,20,5]
 for i in range(1,len(requestProbs)):
     requestProbs[i] += requestProbs[i-1]
 
-def generateRequest():
-    prob = random.random()
-    for i in requestProbs:
-        if prob*100 < i:
-            return i
+
+class Request:
+    def __init__(self, arrivalTime) -> None:
+        self.arrivalTime = arrivalTime
+        self.Type = self.generateType()
+        # self.completed = False
+
+    def generateType():
+        prob = random.random()
+        for i in len(requestProbs):
+            if prob*100 < requestProbs[i]:
+                return typeRequests[i]
+
+class ServiceInstance:
+    def __init__(self, Type) -> None:
+        self.Type = Type
+        self.active = False
     
+    def start(self):
+        self.active = True
+        
+        
+    
+    
+class Service:
+    def __init__(self,instanceNumber, Type) -> None:
+        self.queue = []
+        self.instances = self.generateInstances(instanceNumber)
+        
+        self.Type = Type
+    def generateInstances(instanceNumber):
+        result = []
+        for _ in range(instanceNumber):
+            result.append(ServiceInstance())
+    def passTime():
+
+
 
 service_instances = (list(map(int, input().split())))
 request_rate = int(input())
 duration = int(input())
 max_time = (list(map(int, input().split()))) #optional
 
+services = []
+for i, instanceNum in enumerate(service_instances):
+    services.append(Service(instanceNum, microServices(i+1)))
+
+time = 0
 
 for _ in range(duration):
     for _ in range(request_rate):
-        
+        request = Request(time)
+
+    
+    time += 1
